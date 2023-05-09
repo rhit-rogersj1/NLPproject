33f4031f94fd97aafa4ad904b1f0d92625fcb8cd
rhit-biswalt <biswalt@rose-hulman.edu>
2023-05-09 16:23:53 -0400

    Made changes to how web works

--- web/APIServer/app.py
+++ web/APIServer/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
 import os
 
+def processHelper(uid):
+    #will check if audio sample already transcribed/summarized
+    #if one of the above is not done then it will do so and store in user's respective firebase storage with id
+    #id linked to original audio sample maybe using date and time uploaded
+    #returns null if no audio sample, or does processing
+    #everytime user uploads clears old stuff
+    return 'I was called'
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # Check if the post request has the file part
@@ -21,5 +29,11 @@ def upload_file():
             return 'Invalid file format'
     return render_template('upload.html')
 
+@app.route('/process', methods=['GET'])
+def process():
+    uid = request.args.get('uid')
+    processHelper(uid)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
